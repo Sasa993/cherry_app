4b758b146553fd8ac37320173e860404c3d01813
sasa993 <jovicic.sasa@hotmail.com>
2019-06-22 05:09:16 +0200

    Done with connecting FE-Sidebar with our Backend

---
+++ books/static/script/script.js
@@ -0,0 +1,41 @@
+jQuery(function ($) {
+    $(".sidebar-dropdown > a").click(function () {
+        $(".sidebar-submenu").slideUp(200);
+        if (
+            $(this)
+            .parent()
+            .hasClass("active")
+        ) {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this)
+                .parent()
+                .removeClass("active");
+        } else {
+            $(".sidebar-dropdown").removeClass("active");
+            $(this)
+                .next(".sidebar-submenu")
+                .slideDown(200);
+            $(this)
+                .parent()
+                .addClass("active");
+        }
+    });
+    
+    $("#show-sidebar").click(function () {
+        $(".page-wrapper").addClass("toggled");
+    });
+    $("#close-sidebar").click(function () {
+        $(".page-wrapper").removeClass("toggled");
+    });
+
+    //Progress bar
+     $('.percent').percentageLoader({
+                valElement: 'p',
+                strokeWidth: 20,
+                bgColor: 'transparent',
+                ringColor: '#d53f3f',
+                textColor: '#ffffff',
+                fontSize: '16px',
+                fontWeight: 'normal'
+            });
+});
