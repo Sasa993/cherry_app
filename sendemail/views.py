cfce4b9f73135efa05ce0998ea310736ef5b71c9
Sasa993 <jovicic.sasa@hotmail.com>
2019-06-26 06:04:01 +0200

    Done with connection FE-Login Form and FE-Password Reset/Forgot Password with our Back-End. Had to use some javascript to do some minor changes on DOM. Deleted login.js because we can't use any sliding (due to our 2FE wizard form). Next step is to finish FE-Use Backup Tokens

--- sendemail/views.py
+++ sendemail/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 
+
 def email_main(request):
     if request.method == 'GET':
         form = ContactForm()
@@ -16,13 +17,14 @@ def email_main(request):
             html_content = f"<strong>{cleaned_form['name']}</strong> has sent a message regarding our Workflow Software.<br>His email is <strong>{cleaned_form['from_email']}</strong> and the messsage is:<br><br><strong>{cleaned_form['message']}</strong>"
             from_email = f"{cleaned_form['from_email']}"
             try:
-            	msg = EmailMultiAlternatives(subject, message, from_email, ['cherryfinanceapp@gmail.com'])
-            	msg.attach_alternative(html_content, "text/html")
-            	msg.send()
+                msg = EmailMultiAlternatives(subject, message, from_email, ['cherryfinanceapp@gmail.com'])
+                msg.attach_alternative(html_content, "text/html")
+                msg.send()
             except BadHeaderError:
                 return HttpResponse("Email hasn't been sent")
             return redirect('success/')
     return render(request, "email.html", {'form': form})
+
 
 def email_success(request):
     return render(request, "email_success.html")
