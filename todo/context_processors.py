009a8c4195455f882c4dd87328dac4002f1cfdd3
Sasa993 <jovicic.sasa@hotmail.com>
2019-06-28 08:22:44 +0200

    fixed minor stuff

--- todo/context_processors.py
+++ todo/context_processors.py
@@ -7,31 +7,20 @@ def add_variable_to_context(request):
 		if request.user.is_superuser:
 			all_tasks = Task.objects.all().count()
 			all_tasks_completed = Task.objects.filter(completed=1).count()
-
-			try:
-				all_tasks_percentage = (all_tasks_completed * 100) / all_tasks
-			except ZeroDivisionError:
-				all_tasks_percentage = 0
-
-			return {
-				'all_tasks': all_tasks,
-				'all_tasks_completed': all_tasks_completed,
-				'all_tasks_percentage': all_tasks_percentage,
-			}
 		else:
 			all_tasks = Task.objects.filter(assigned_to=request.user).count()
 			all_tasks_completed = Task.objects.filter(assigned_to=request.user, completed=1).count()
 
-			try:
-				all_tasks_percentage = (all_tasks_completed * 100) / all_tasks
-			except ZeroDivisionError:
-				all_tasks_percentage = 0
+		try:
+			all_tasks_percentage = (all_tasks_completed * 100) / all_tasks
+		except ZeroDivisionError:
+			all_tasks_percentage = 0
 
-			return {
-				'all_tasks': all_tasks,
-				'all_tasks_completed': all_tasks_completed,
-				'all_tasks_percentage': all_tasks_percentage,
-			}
+		return {
+			'all_tasks': all_tasks,
+			'all_tasks_completed': all_tasks_completed,
+			'all_tasks_percentage': all_tasks_percentage,
+		}
 	else:
 		return {
 			'': ''
