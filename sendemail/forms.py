c58c7bdfdd1bac359d9da80703ad1479163a952c
sasa993 <jovicic.sasa@hotmail.com>
2019-07-27 22:52:15 +0200

    Done with Connect FE-BE Contact Us Form

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control rounded_top_corner', 'placeholder': 'Your Name'}))
+    from_email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your E-Mail'}))
+    subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}))
+    message = forms.CharField(required=True, widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message...'}))
