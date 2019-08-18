378be0c57fa1a150c7d3640c7360f4a8fdb2fa00
Sasa993 <jovicic.sasa@hotmail.com>
2019-08-18 16:44:49 +0200

    done with edit templates - started with details templates

--- uploaded_books/models.py
+++ uploaded_books/models.py
@@ -4,30 +4,6 @@ from books.models import Book
 import os
 
 
-# class EBook(models.Model):
-# 	name = models.CharField(max_length=50, default='E-Book', editable=False)
-# 	title = models.CharField(max_length=100)
-# 	uploaded_at = models.DateTimeField(auto_now_add=True, blank=True)
-# 	modified_at = models.DateTimeField(auto_now=True, blank=True)
-# 	source_file = models.FileField()
-# 	epub_file = models.FileField()
-# 	mobi_file = models.FileField()
-# 	cover_file = models.FileField()
-
-# 	def __str__(self):
-# 		return f"{self.title}"
-
-# 	def _testiramo(cls, self):
-# 		return f"{self.name}"
-
-# 	# overriding the default "delete" method in order to delete all the uploaded files of the certain book
-# 	def delete(self, *args, **kwargs):
-# 		os.remove(os.path.join(settings.MEDIA_ROOT, self.source_file.name))
-# 		os.remove(os.path.join(settings.MEDIA_ROOT, self.epub_file.name))
-# 		os.remove(os.path.join(settings.MEDIA_ROOT, self.mobi_file.name))
-# 		os.remove(os.path.join(settings.MEDIA_ROOT, self.cover_file.name))
-# 		super(EBook, self).delete(*args, **kwargs)
-
 class EBook(Book):
 	name = models.CharField(max_length=50, default='E-Book', editable=False)
 	source_file = models.FileField()
