7fa3d57ae046ec4e11f63260cd05426fc2f79a34
sasa993 <jovicic.sasa@hotmail.com>
2019-11-30 10:50:20 +0100

    Done with 'Double .png extensions'... I rewrote the templatetag - shorten_file_name...

--- uploaded_books/templatetags/shorten_file_name.py
+++ uploaded_books/templatetags/shorten_file_name.py
@@ -7,6 +7,10 @@ register = template.Library()
 @register.filter
 @stringfilter
 def shorten_file_name(value):
-	extension = value.split('.')[-1]
+	# splits the string into a list and takes the last element - which is the extension, and adds "." in front of it
+	extension = '.' + value.split('.')[-1]
+	# takes everyother element but removes the extension - we could make it take the first element, but there might be a filename with dots mixed with the name
+	value_without_ext = value.replace(extension, "")
 
-	return value[:15] + "." + extension
+	# returns the first 15 characters of the file name and the extension at the end
+	return value_without_ext[:15] + extension
